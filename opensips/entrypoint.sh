10a0d3fa27fbad68e819f774119786064f688ffe
Norm Brandinger <n.brandinger@gmail.com>
2023-04-27 13:43:01 -0400

    Fix: Reduce the log level from DEBUG and add a couple more UDP workers

--- opensips/entrypoint.sh
+++ opensips/entrypoint.sh
@@ -5,11 +5,11 @@ echo "Container's IP address: `awk 'END{print $1}' /etc/hosts`"
 IP=$(awk 'END{print $1}' /etc/hosts)
 
 sed \
-  -e "s|{log_level}|4|g" \
+  -e "s|{log_level}|3|g" \
   -e "s|{xlog_level}|3|g" \
   -e "s|{log_stderror}|yes|g" \
   -e "s|{log_facility}|LOG_LOCAL0|g" \
-  -e "s|{udp_workers}|2|g" \
+  -e "s|{udp_workers}|4|g" \
   -e "s|{tcp_workers}|4|g" \
   -e "s|0.0.0.0|$IP|g" \
   -e "s|/run/opensips|/usr/local/etc/opensips|g" \
