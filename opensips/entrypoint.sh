cc71505cfeee69998e2a7a1025cf96055f098dc3
Norm Brandinger <n.brandinger@gmail.com>
2023-04-26 23:18:18 -0400

    Fix: Add support for SQLite

--- opensips/entrypoint.sh
+++ opensips/entrypoint.sh
@@ -15,4 +15,49 @@ sed \
   -e "s|/run/opensips|/usr/local/etc/opensips|g" \
   -i /usr/local/etc/opensips/opensips.cfg
 
+if [[ ! -e opensips.sqlite ]]; then
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/standard-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/acc-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/alias_db-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/auth_db-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/authjwt-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/avpops-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/b2b-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/b2b_sca-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/cachedb_sql-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/call_center-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/carrierroute-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/closeddial-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/clusterer-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/cpl-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/dialog-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/dialplan-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/dispatcher-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/domain-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/domainpolicy-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/drouting-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/emergency-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/fraud_detection-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/freeswitch_scripting-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/group-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/imc-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/load_balancer-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/msilo-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/permissions-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/presence-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/qrouting-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/ratecacher-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/registrant-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/rls-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/rtpengine-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/rtpproxy-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/smpp-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/speeddial-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/tcp_mgm-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/tls_mgm-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/tracer-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/userblacklist-create.sql
+  sqlite3 opensips.sqlite < /usr/local/src/opensips/scripts/sqlite/usrloc-create.sql
+fi
+
 exec "$@"
